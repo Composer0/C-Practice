c392aba88551d0338ce28886ec0b4f608843405d
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-08-03 21:53:07 -0400

    Hello World being programmed into C#. I am already seeing similarities between JavaScript and C# when it comes to the use of data/attributes and methods/functions.

---
+++ program.cs
@@ -0,0 +1,16 @@
+using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+
+namespace HellowWorld
+{
+    class Program
+    {
+        static void Main(string[] args) //Main has to be capital to be found by CLR.
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
